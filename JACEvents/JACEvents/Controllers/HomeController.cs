d8730cd6eac64af0f0af06d5de972eca9282c287
Awoyemihafeez <haviz942@gmail.com>
2020-10-18 20:06:58 +0100

    First Commit

---
+++ JACEvents/JACEvents/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JACEvents.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            using (JACEntities dc = new JACEntities())
+            {
+                var events = dc.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+
+        [HttpPost]
+        public JsonResult SaveEvent(Event e)
+        {
+            var status = false;
+            using (JACEntities dc = new JACEntities())
+            {
+                if (e.EventId > 0)
+                {
+                    //Update the event
+                    var v = dc.Events.Where(a => a.EventId == e.EventId).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Description = e.Description;
+                        v.ModelNumber = e.ModelNumber;
+                        v.CustomerName = e.CustomerName;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.IsFullDay = e.IsFullDay;
+                        v.Color = e.Color;
+                    }
+                }
+                else
+                {
+                    dc.Events.Add(e);
+                }
+                dc.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+          //var eventID= int.Parse(seventID);
+            var status = false;
+            using (JACEntities dc = new JACEntities())
+            {
+                var v = dc.Events.Where(a => a.EventId == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Events.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+    }
+}
